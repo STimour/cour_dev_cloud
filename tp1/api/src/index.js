c44392989ac8f6f529fd95348017869791c70b40
Dorianyloj <dorian25210@gmail.com>
2026-03-26 12:45:25 +0100

    Ajout docker-compose

--- tp1/api/src/index.js
+++ tp1/api/src/index.js
@@ -81,7 +81,7 @@ app.delete("/notes/:id", async (req, res) => {
 // Start server
 // =======================
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`API running on port ${port}`);
